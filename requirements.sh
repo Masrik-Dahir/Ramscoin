606626f4f7bc83b1689574aa1870dc3519c33cf8
Masrik Dahir <dahirma@vcu.edu>
2022-04-26 09:44:16 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,8 @@
-pip install pipenv
+pip3 install pipenv
 pipenv install --dev
-pip install uuid
-pip install urllib3
-pip install jsonlib
-pip install hashlib
+pip3 install uuid
+pip3 install urllib3
+pip3 install jsonlib
+pip3 install hashlib
+pip3 install Flask
 pipenv run python -m unittest
